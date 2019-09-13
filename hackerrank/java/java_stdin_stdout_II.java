ffdffe647b33bf0a05afc67f06cd5374747a4872
ashneg <500060167@stu.upes.ac.in>
2019-09-14 05:18:49 +0530

    Codes added

---
+++ hackerrank/java/java_stdin_stdout_II.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
