eef3e4f467e1b698df33190eb9ad89db68c3e9f9
ashneg <500060167@stu.upes.ac.in>
2020-04-15 10:21:33 +0530

    Codes Added

---
+++ codeforces/cpp/Problem_1335A.cpp
@@ -0,0 +1,13 @@
+    #include<bits/stdc++.h>
+    using namespace std; 
+     
+    int main() 
+    {
+    	int t, n;
+    	scanf("%d",&t);
+    	while(t--) {
+    		scanf("%d",&n);
+    		printf("%d\n",(n-1)/2);
+    	}
+    	return 0;
+    }
